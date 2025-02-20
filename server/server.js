c320325470606aac8ebd60f0744ed9db7fde7e17
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-02-20 10:15:24 +0100

    add middlewares

--- server/server.js
+++ server/server.js
@@ -2,13 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 const fantaRouter = require('./routers/fantaRouter')
+const errorsHandler = require('./middlewares/errorsHandler')
+const notFound = require('./middlewares/notFound')
 
 app.get('/', (req, res) => {
   res.send('Fantacalcio')
 })
 
+
 //Rotta
 app.use('/api/fantacalcio', fantaRouter)
+
+
+//Middlewares per errori
+app.use(errorsHandler)
+app.use(notFound)
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
