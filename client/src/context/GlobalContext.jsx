b5f84af4ecc353c0220184091de01be924ae5aac
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-02-20 10:23:27 +0100

    add GlobalContext

---
+++ client/src/context/GlobalContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useEffect, useState } from "react"
+import axios from 'axios'
+
+export const GlobalContext = createContext()
+
+export default function GlobalProvider({ childern }) {
+  const [partecipanti, setPartecipanti] = useState([])
+
+
+  function fetchPartecipanti() {
+    axios.get('http://localhost:3000/api/fantacalcio')
+      .then(res => setPartecipanti(res.data))
+      .catch(err => console.error(err))
+  }
+
+  useEffect(() => {
+    fetchPartecipanti()
+  }, [])
+
+  return (
+    <GlobalContext.Provider value={{ partecipanti, setPartecipanti }}>
+      {childern}
+    </GlobalContext.Provider>
+  )
+
+
+
+}
